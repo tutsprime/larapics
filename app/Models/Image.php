24fdb46c4ea1d51bd2bf52c2b633b80a7ee45c27
TutsPrime <tutsprimedotcom@gmail.com>
2022-02-10 17:34:18 +0700

    Using file storage to fill file and dimension columns

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Image extends Model
 {
     use HasFactory;
+
+    public static function makeDirectory()
+    {
+        $subFolder = date('Y/m/d');
+        Storage::makeDirectory('images/' . $subFolder);
+    }
+
+    public static function getDimension($image)
+    {
+        [$width, $height ] = getimagesize(Storage::path($image));
+        return $width . "x" . $height;
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Image;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Image::factory(6)->create();
+        $images = Storage::allFiles('images');
+
+        foreach ($images as $image)
+        {
+            if (strpos($image, ".DS_Store")) continue;
+
+            Image::factory()->create([
+                'file' => $image,
+                'dimension' => Image::getDimension($image)
+            ]);
+        }
     }
 }
