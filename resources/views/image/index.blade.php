24368797109e59b2bba95b2d9bf5d5f903c1d9d9
TutsPrime <tutsprimedotcom@gmail.com>
2022-02-19 19:08:17 +0700

    Build application layout using blade component

--- resources/views/image/index.blade.php
+++ resources/views/image/index.blade.php
@@ -1,21 +1,23 @@
-<h1>All images</h1>
-
-<a href="{{ route('images.create') }}">Upload Image</a>
-
-@if ($message = session('message'))
-    <div>{{ $message }}</div>
-@endif
-
-@foreach ($images as $image)
-    <div>
-        <a href="{{ $image->permalink() }}">
-            <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" width="300">
-        </a>
+<x-layout title="Discover free images">
+    <h1>All images</h1>
+    
+    <a href="{{ route('images.create') }}">Upload Image</a>
+    
+    @if ($message = session('message'))
+        <div>{{ $message }}</div>
+    @endif
+    
+    @foreach ($images as $image)
         <div>
-            <a href="{{ $image->route('edit') }}">Edit</a> | 
-            <x-form action="{{ $image->route('destroy') }}" method="DELETE" style="display: inline">
-                <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
-            </x-form>
+            <a href="{{ $image->permalink() }}">
+                <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" width="300">
+            </a>
+            <div>
+                <a href="{{ $image->route('edit') }}">Edit</a> |
+                <x-form action="{{ $image->route('destroy') }}" method="DELETE" style="display: inline">
+                    <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
+                </x-form>
+            </div>
         </div>
-    </div>
-@endforeach
+    @endforeach
+</x-layout>
