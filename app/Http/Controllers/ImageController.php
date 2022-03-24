0f5b17855965aeae659ef1ad2a8ddbc7dbd9aa2e
TutsPrime <tutsprimedotcom@gmail.com>
2022-03-24 21:51:48 +0700

    Using bootstrap 5 pagination

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,7 +10,7 @@ class ImageController extends Controller
 {
     public function index()
     {
-        $images = Image::published()->latest()->paginate(15);
+        $images = Image::published()->latest()->paginate(15)->withQueryString();
 
         return view('image.index', compact('images'));
     }
