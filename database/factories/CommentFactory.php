a216d6d69a18a36374117bf429e739a01d973dd7
TutsPrime <tutsprimedotcom@gmail.com>
2023-05-14 16:16:59 +0700

    Attach fake comments for images

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -17,7 +17,7 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'body' => $this->faker->paragraph()
         ];
     }
 }
